9c0d6b71a4ddf44d18e23c1a8175eba5f5f0d15f
nicholasmiltondev <nicholassmilton@gmail.com>
2020-01-10 18:51:16 -0800

    Initial commit

---
+++ functions/index.js
@@ -0,0 +1,29 @@
+const functions = require('firebase-functions');
+const app = require('express')();
+const FBAuth = require('./util/fbAuth');
+
+const { 
+    getAllScreams, 
+    postOneScream
+} = require('./handlers/screams');
+
+const { 
+    signup, 
+    login, 
+    uploadImage,
+    addUserDetails,
+    getAuthenticatedUser
+} = require('./handlers/users');
+
+// Scream routes
+app.get('/screams', getAllScreams);
+app.post('/scream', FBAuth, postOneScream);
+
+// Users route
+app.post('/signup', signup);
+app.post('/login', login);
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
+
+exports.api = functions.https.onRequest(app);
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyCip3lnIjyLh1-727eiQu1zZ1a3Ulbpe5I",
+    authDomain: "artinvancouver-58f67.firebaseapp.com",
+    databaseURL: "https://artinvancouver-58f67.firebaseio.com",
+    projectId: "artinvancouver-58f67",
+    storageBucket: "artinvancouver-58f67.appspot.com",
+    messagingSenderId: "541347844185",
+    appId: "1:541347844185:web:1ef9475249adb5ff20a148",
+    measurementId: "G-FZQY8L5KN9"
+  };
